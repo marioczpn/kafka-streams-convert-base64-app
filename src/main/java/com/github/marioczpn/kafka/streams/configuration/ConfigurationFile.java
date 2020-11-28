47a6fde792f13ef116fa460230b1735710e7ffc1
Mario Ponciano <marioczpn@gmail.com>
2020-11-28 08:45:07 -0300

    Adding unit test for ConfigurationFile

--- src/main/java/com/github/marioczpn/kafka/streams/configuration/ConfigurationFile.java
+++ src/main/java/com/github/marioczpn/kafka/streams/configuration/ConfigurationFile.java
@@ -1,6 +1,7 @@
 package com.github.marioczpn.kafka.streams.configuration;
 
 import com.github.marioczpn.kafka.streams.constants.Constants;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsConfig;
 import org.slf4j.Logger;
@@ -29,7 +30,9 @@ public class ConfigurationFile {
      */
     public Properties buildStreamsProperties(Properties envProps) {
         if (envProps == null) {
-            logger.error("Environment properties is empty and this step won't be executed.");
+            final String errorMsg = "Environment properties is empty and this step won't be executed.";
+            logger.error(errorMsg);
+            throw new IllegalArgumentException(errorMsg);
         }
 
         logger.info("Building the streams properties... ");
@@ -80,7 +83,7 @@ public class ConfigurationFile {
      */
     private InputStream readConfigFile(String fileName) throws FileNotFoundException {
         InputStream input = null;
-        if (fileName.isBlank()) {
+        if (StringUtils.isBlank(fileName)) {
             logger.info("Setting INTERNAL resource file.");
             input = getClass().getClassLoader().getResourceAsStream(Constants.INTERNAL_CONFIG_FILE_FROM_RESOURCES);
         } else {
@@ -99,12 +102,22 @@ public class ConfigurationFile {
     public Properties fromEnv() {
         logger.info("Setting environment variables");
 
+        final String appId = System.getenv(Constants.APPLICATION_ID_ENVVAR);
+        final String clientId = System.getenv(Constants.CLIENT_ID_ENVVAR);
+        final String bootStrapServer = System.getenv(Constants.BOOTSTRAP_SERVERS_ENVVAR);
+        final String inputTopic =  System.getenv(Constants.INPUT_TOPIC_NAME_ENVVAR);
+        final String streamsOutputTopicName = System.getenv(Constants.STREAMS_OUTPUT_TOPIC_NAME_ENVVAR);
+        if(StringUtils.isBlank(appId) || StringUtils.isBlank(clientId) || StringUtils.isBlank(bootStrapServer) || StringUtils.isBlank(inputTopic) || StringUtils.isBlank(streamsOutputTopicName)) {
+            logger.error("Environment variables are *EMPTY*. You can set an environment variable.");
+            return null;
+        }
+
         Properties envProps = new Properties();
-        envProps.put(Constants.APPLICATION_ID, System.getenv(Constants.APPLICATION_ID_ENVVAR));
-        envProps.put(Constants.CLIENT_ID, System.getenv(Constants.CLIENT_ID_ENVVAR));
-        envProps.put(Constants.BOOTSTRAP_SERVERS, System.getenv(Constants.BOOTSTRAP_SERVERS_ENVVAR));
-        envProps.put(Constants.INPUT_TOPIC_NAME, System.getenv(Constants.INPUT_TOPIC_NAME_ENVVAR));
-        envProps.put(Constants.STREAMS_OUTPUT_TOPIC_NAME, System.getenv(Constants.STREAMS_OUTPUT_TOPIC_NAME_ENVVAR));
+        envProps.put(Constants.APPLICATION_ID, appId);
+        envProps.put(Constants.CLIENT_ID, clientId);
+        envProps.put(Constants.BOOTSTRAP_SERVERS, bootStrapServer);
+        envProps.put(Constants.INPUT_TOPIC_NAME, inputTopic);
+        envProps.put(Constants.STREAMS_OUTPUT_TOPIC_NAME, streamsOutputTopicName);
 
         logger.info("App is using the environment variable: " + envProps.toString());
         return envProps;
---
+++ src/test/java/com/github/marioczpn/kafka/streams/configuration/ConfigurationFileTest.java
@@ -0,0 +1,135 @@
+package com.github.marioczpn.kafka.streams.configuration;
+
+import com.github.marioczpn.kafka.streams.constants.Constants;
+import com.github.marioczpn.kafka.streams.processor.Base64ProcessorTest;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsConfig;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class ConfigurationFileTest {
+    private static final Logger logger = LoggerFactory.getLogger(Base64ProcessorTest.class.getName());
+
+
+    @Test
+    void shouldNotAllowNullThrowsIllegalArgumentException() {
+        ConfigurationFile configFile = new ConfigurationFile();
+        Assertions.assertThrows(IllegalArgumentException.class,  () -> {
+           configFile.buildStreamsProperties(null);
+        });
+    }
+
+    @Test
+    void shouldBuildStreamsProperties() {
+        Properties props = new Properties();
+        props.put(Constants.INPUT_TOPIC_NAME, "test");
+        props.put(Constants.STREAMS_OUTPUT_TOPIC_NAME, "dummy:1234");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "appId");
+        props.put(StreamsConfig.CLIENT_ID_CONFIG, "clientId");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.ByteArray().getClass().getName());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+
+        ConfigurationFile configFile = new ConfigurationFile();
+        Properties streamsProperties = configFile.buildStreamsProperties(props);
+
+        Assertions.assertNotNull(streamsProperties);
+    }
+
+    @Test
+    void shouldLoadEnvPropertiesFromResourceFile() throws Exception {
+        Map<String, String> envVariablesMap = new HashMap<>();
+        envVariablesMap.put(Constants.APPLICATION_ID_ENVVAR, "");
+        this.setEnvironmentVariable(envVariablesMap);
+
+        ConfigurationFile configFile = new ConfigurationFile();
+        Properties streamsProperties = configFile.loadEnvProperties(null);
+
+        Assertions.assertNotNull(streamsProperties);
+    }
+
+    @Test
+    void shouldLoadEnvPropertiesFromConfigFile() throws IOException {
+
+        ConfigurationFile configFile = new ConfigurationFile();
+        Properties streamsProperties = configFile.loadEnvProperties("config/config.properties");
+
+        Assertions.assertNotNull(streamsProperties);
+    }
+
+    @Test
+    void shouldReturnNullNoEnvironmentVariableDefined() throws Exception {
+        Map<String, String> envVariablesMap = new HashMap<>();
+        envVariablesMap.put(Constants.APPLICATION_ID_ENVVAR, "");
+        this.setEnvironmentVariable(envVariablesMap);
+
+        ConfigurationFile configFile = new ConfigurationFile();
+        Properties envVariables = configFile.fromEnv();
+
+        Assertions.assertNull(envVariables);
+    }
+
+    @Test
+    void shouldLoadEnvPropertiesFromEnvironmentVariable() throws Exception {
+        Map<String, String> envVariablesMap = new HashMap<>();
+        envVariablesMap.put(Constants.APPLICATION_ID_ENVVAR, "appId");
+        envVariablesMap.put(Constants.CLIENT_ID_ENVVAR, "clientId");
+        envVariablesMap.put(Constants.BOOTSTRAP_SERVERS_ENVVAR, "bootStrapServer");
+        envVariablesMap.put(Constants.INPUT_TOPIC_NAME_ENVVAR, "inputTopic");
+        envVariablesMap.put(Constants.STREAMS_OUTPUT_TOPIC_NAME_ENVVAR, "streamsOutputTopicName");
+        this.setEnvironmentVariable(envVariablesMap);
+
+        ConfigurationFile configFile = new ConfigurationFile();
+        Properties streamsProperties = configFile.loadEnvProperties(null);
+
+        Assertions.assertNotNull(streamsProperties);
+    }
+
+
+    /**
+     * This method is setting envinroment variable programatically.
+     *
+     * @param mapEnvVariable
+     * @throws Exception
+     */
+    private static void setEnvironmentVariable(Map<String, String> mapEnvVariable) throws Exception {
+        try {
+
+            Class<?> processEnvironmentClass = Class.forName("java.lang.ProcessEnvironment");
+            Field theEnvironmentField = processEnvironmentClass.getDeclaredField("theEnvironment");
+            theEnvironmentField.setAccessible(true);
+
+            Map<String, String> env = (Map<String, String>) theEnvironmentField.get(null);
+            env.putAll(mapEnvVariable);
+
+            Field theCaseInsensitiveEnvironmentField = processEnvironmentClass.getDeclaredField("theCaseInsensitiveEnvironment");
+            theCaseInsensitiveEnvironmentField.setAccessible(true);
+
+            Map<String, String> cienv = (Map<String, String>)     theCaseInsensitiveEnvironmentField.get(null);
+            cienv.putAll(mapEnvVariable);
+
+        } catch (NoSuchFieldException e) {
+            Class[] classes = Collections.class.getDeclaredClasses();
+            Map<String, String> env = System.getenv();
+            for(Class cl : classes) {
+                if("java.util.Collections$UnmodifiableMap".equals(cl.getName())) {
+                    Field field = cl.getDeclaredField("m");
+                    field.setAccessible(true);
+                    Object obj = field.get(env);
+                    Map<String, String> map = (Map<String, String>) obj;
+                    map.clear();
+                    map.putAll(mapEnvVariable);
+                }
+            }
+        }
+    }
+}
